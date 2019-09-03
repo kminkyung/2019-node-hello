5488648155d947d1155f450c0bee67797597e1be
kminkyung <mminkyung.k@gmail.com>
2019-09-03 15:56:40 +0900

    app.backup 생성/mysql2 배우는 중

--- app.backup-20190903.js
+++ app.backup-20190903.js
@@ -10,10 +10,16 @@ const sqlErr = db.sqlErr;
 const sqlExec = db.sqlExec;
 const util = require("./modules/util");
 
+// const nameMaker = require('./modules/test');
+
 // listen() : express 의 method 이며, 서버를 구동시킨다.
 app.listen(8000, () => {
 	console.log("http://localhost:8000");
+/* 	console.log(nameMaker.firstName);
+	console.log(nameMaker.lastName);
+	console.log(nameMaker.fullName()); */
 });
+
 
 // Router (길잡이)
 app.use(bodyParser.urlencoded({extended: false}));
@@ -26,6 +32,33 @@ app.get("/hello", (req, res) => {
 	res.send(html);
 });
 
+
+/* app.post("/gbook_save", (req, res) => {
+	var comment = req.body.comment;
+	db.conn.getConnection((err, connect)=>{
+		if(err) res.send("DB접속 오류가 발생했습니다.")
+		else {
+			var sql = 'INSERT INTO gbook SET comment=?, wtime=?'; //query문
+			var vals = [comment, util.dspDate(new Date())]; //첫번째 물음표 값을 첫번째 인자에, 두번째 물음표 값을 두번째 인자에 vals 배열화
+			connect.query(sql, vals, (err, result) => {
+				connect.release();
+				if(err)	res.send("데이터 저장에 실패했습니다.");
+				else {
+					res.send("데이터가 처리되었습니다.");
+				}
+			});
+		}
+	}); //접근이 성공하면 함수를 실행. params 은 위처럼 두 개 넣는 걸로 매뉴얼로 정해져있음.
+}); 
+async function getData(sql, vals) {
+	const connect = await conn.getConnection(async conn => conn);
+	const data = await connect.query(sql, vals); //결과를 주기전에 55라인에서 홀딩
+	connect.release()
+	return data;
+}
+
+*/
+
 // async / await 패턴
 app.post("/gbook_save", (req, res) => {
 	var comment = req.body.comment;
@@ -36,6 +69,8 @@ app.post("/gbook_save", (req, res) => {
 		res.send(data);
 	}).catch(sqlErr);
 });
+
+
 
 // Semantic 방식
 app.get("/gbook/:page", (req, res) => {
--- modules/mysql_conn.js
+++ modules/mysql_conn.js
@@ -1,14 +1,32 @@
-const mysql = require("mysql"); // npm i -S mysql 로 설치한 모듈 불러오기
-const conn = mysql.createPool({
+// const mysql = require("mysql"); // npm i -S mysql 로 설치한 모듈 불러오기
+const mysql = require("mysql2/promise"); // npm i -S mysql2 로 설치한 모듈 불러오기
+const sqlPool = mysql.createPool({
 	host: "127.0.0.1",
 	user: "minkyung",
 	password: "000000",
 	port: 3306,
 	database: "minkyung",
+	waitForConnections: true,
+	queueLimit: 0,
 	connectionLimit: 10 //동시접속자 수, 높으면 높을 수록 서버 메모리가 많이 필요하다.
 });
 
+const sqlErr = err => {
+	console.log(err);
+}
+
+const sqlExec = async (sql, vals) => {
+	const connect = await sqlPool.getConnection(async conn => conn);
+	const data = await connect.query(sql, vals); //결과를 주기전에 55라인에서 홀딩
+	connect.release()
+	return data;
+}
+
+
 module.exports = {
  mysql,
- conn
+ sqlPool,
+ sqlErr,
+ sqlExec
 };
+
